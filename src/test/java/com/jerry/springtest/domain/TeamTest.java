29b9545c27b0bbe4396a2b4e034334fa7aa08e94
jerry92k <jens92.k@gmail.com>
2022-01-30 22:54:11 +0900

    test: 불필요 import 삭제

--- src/test/java/com/jerry/springtest/domain/TeamTest.java
+++ src/test/java/com/jerry/springtest/domain/TeamTest.java
@@ -6,9 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import com.jerry.springtest.domain.Member;
-import com.jerry.springtest.domain.Team;
-
 @DataJpaTest
 class TeamTest {
 

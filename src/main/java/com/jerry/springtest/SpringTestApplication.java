6240da1e6cb54a5800473761d2d01f4c61fb1da2
jerry92k <jens92.k@gmail.com>
2022-06-08 08:22:36 +0900

    - 프로젝트 구성 추가

---
+++ src/main/java/com/jerry/springtest/SpringTestApplication.java
@@ -0,0 +1,15 @@
+package com.jerry.springtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+@ServletComponentScan
+public class SpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jerry/springtest/SpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerry.springtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
